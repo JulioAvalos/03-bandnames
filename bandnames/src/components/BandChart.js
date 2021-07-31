26472afc0972fbb6f9322422d45928392d42883c
JulioAvalos <julioavalos2@msn.com>
2021-07-31 11:24:54 -0600

    getting bands from socket to display votes in the bar chart

--- bandnames/src/components/BandChart.js
+++ bandnames/src/components/BandChart.js
@@ -1,48 +1,61 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { SocketContext } from "../context/SocketContext";
 import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
+let myChart;
 
 export const BandChart = () => {
-    useEffect(() => {
-        const ctx = document.getElementById("myChart");
-        var myChart = new Chart(ctx, {
-          type: "bar",
-          data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [
-              {
-                label: "# of Votes",
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                  "rgba(255, 99, 132, 0.2)",
-                  "rgba(54, 162, 235, 0.2)",
-                  "rgba(255, 206, 86, 0.2)",
-                  "rgba(75, 192, 192, 0.2)",
-                  "rgba(153, 102, 255, 0.2)",
-                  "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                  "rgba(255, 99, 132, 1)",
-                  "rgba(54, 162, 235, 1)",
-                  "rgba(255, 206, 86, 1)",
-                  "rgba(75, 192, 192, 1)",
-                  "rgba(153, 102, 255, 1)",
-                  "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-              },
+  const { socket } = useContext(SocketContext);
+
+  useEffect(() => {
+    socket.on("current-bands", (bands) => {
+        crearGrafica(bands);
+    });
+    return () => socket.off("current-bands");
+  }, [socket]);
+
+  const crearGrafica = (bands = []) => {
+    const ctx = document.getElementById("myChart");
+    if(typeof myChart !== 'undefined') myChart.destroy();
+    myChart = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: bands.map(band => band.name),
+        datasets: [
+          {
+            label: "# of Votes",
+            data: bands.map(band => band.votes),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
             ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
           },
-          options: {
-            indexAxis: 'y',
-            scales: {
-              x: {
-                stacked: true
-              },
-            },
+        ],
+      },
+      options: {
+        animation: false,
+        indexAxis: "y",
+        scales: {
+          x: {
+            stacked: true,
           },
-        });
-      }, []);
+        },
+      },
+    });
+  };
 
   return <canvas id="myChart"></canvas>;
 };
