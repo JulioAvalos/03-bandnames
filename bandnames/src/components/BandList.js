15df8c250ff3a2d24d27eb754387b2bc3aa3636b
JulioAvalos <julioavalos2@msn.com>
2021-07-31 10:45:19 -0600

    refactoring bandlist to apply context

--- bandnames/src/components/BandList.js
+++ bandnames/src/components/BandList.js
@@ -1,11 +1,17 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { SocketContext } from "../context/SocketContext";
 
-export const BandList = ({ data, votar, borrar, cambiarNombre }) => {
-  const [bands, setBands] = useState(data);
+export const BandList = () => {
+  const [bands, setBands] = useState([]);
+
+  const { socket } = useContext(SocketContext);
 
   useEffect(() => {
-    setBands(data);
-  }, [data]);
+    socket.on("current-bands", (bands) => {
+      setBands(bands);
+    });
+    return () => socket.off("current-bands");
+  }, [socket]);
 
   const cambioNombre = (event, id) => {
     const nuevoNombre = event.target.value;
@@ -21,7 +27,15 @@ export const BandList = ({ data, votar, borrar, cambiarNombre }) => {
   };
 
   const onPerdioFoco = (id, nombre) => {
-    cambiarNombre(id, nombre);
+    socket.emit("cambiar-nombre-banda", { id, nombre });
+  };
+
+  const votar = (id) => {
+    socket.emit("votar-banda", id);
+  };
+
+  const borrar = (id) => {
+    socket.emit("borrar-banda", id);
   };
 
   const crearRows = () => {
@@ -44,7 +58,9 @@ export const BandList = ({ data, votar, borrar, cambiarNombre }) => {
           <h3>{band.votes}</h3>
         </td>
         <td>
-          <button className="btn btn-danger" onClick={()=>borrar(band.id)}>Borrar</button>
+          <button className="btn btn-danger" onClick={() => borrar(band.id)}>
+            Borrar
+          </button>
         </td>
       </tr>
     ));
--- bandnames/src/pages/HomePage.js
+++ bandnames/src/pages/HomePage.js
@@ -5,30 +5,8 @@ import { BandAdd } from "../components/BandAdd";
 import { BandList } from "../components/BandList";
 
 function HomePage() {
-
-  // const [bands, setBands] = useState([]);
-
-  // const { socket, online } = useSocket("http://localhost:8080");
-
+  
   const { online } = useContext(SocketContext);
-
-  // useEffect(() => {
-  //   socket.on("current-bands", (bands) => {
-  //     setBands(bands);
-  //   });
-  // }, [socket]);
-
-  // const votar = (id) => {
-  //   socket.emit("votar-banda", id);
-  // };
-
-  // const borrar = (id) => {
-  //   socket.emit("borrar-banda", id);
-  // };
-
-  // const cambiarNombre = (id, nombre) => {
-  //   socket.emit("cambiar-nombre-banda", { id, nombre });
-  // };
 
   return (
     <div className="container">
@@ -48,15 +26,10 @@ function HomePage() {
 
       <div className="row">
         <div className="col-8">
-          {/* <BandList
-            data={bands}
-            votar={votar}
-            borrar={borrar}
-            cambiarNombre={cambiarNombre}
-          /> */}
+          <BandList />
         </div>
         <div className="col-4">
-          {/* <BandAdd /> */}
+          <BandAdd />
         </div>
       </div>
     </div>
