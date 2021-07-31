2caab33787dc4dfcb306a898687b2989ba7959e9
JulioAvalos <julioavalos2@msn.com>
2021-07-31 01:29:05 -0600

    Adding socket context and provider

--- bandnames/src/components/BandAdd.js
+++ bandnames/src/components/BandAdd.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
+import { useSocket } from "../hooks/useSocket";
 
-export const BandAdd = ({crearBanda}) => {
+export const BandAdd = () => {
   const [valor, setValor] = useState("");
+
+  const { socket } = useSocket('http://localhost:8080');
 
   const onSubmit = (event) => {
     event.preventDefault();
+
     if(valor.trim().length > 0 ) {
-      crearBanda(valor);
+      socket.emit("nueva-banda", { nombre: valor });
       setValor('');
     }
+
   };
 
   return (
